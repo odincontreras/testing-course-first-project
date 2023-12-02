ded9f0ab94653206f103b2e816eb9b4fcc30346a
Odin Contreras <odin@foreach.cl>
2023-12-02 15:00:55 -0400

    finish App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("can receive a new user and show it on a list", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // get inputs and button
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  // simulate user input
+  user.type(nameInput, "odin");
+  user.type(emailInput, "odin@gmail");
+  user.click(button);
+
+  // get table cells
+  const nameCell = screen.getByRole("cell", { name: "odin" });
+  const emailCell = screen.getByRole("cell", { name: "odin@gmail" });
+
+  // assertions
+  expect(nameCell).toBeInTheDocument();
+  expect(emailCell).toBeInTheDocument();
 });
